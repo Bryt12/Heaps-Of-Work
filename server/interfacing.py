ecd94bf76c2a3c2fe5821e4407de1f8a26ec075a
Ankur Gupta <rukna1000@gmail.com>
2018-04-22 12:10:17 -0400

    Updated basics for server and created init for interfacer

---
+++ server/interfacing.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+class Interfacer():
+    def __init__(front_end_url, port):
+        self.fe_url = front_end_url
+        self.port = port
+    
+    def send(queue):
+        pass
+
+    def get():
+        pass
---
+++ server/server.py
@@ -0,0 +1,6 @@
+class Server:
+    def __init__():
+        pass
+
+if __name__ == '__main__':
+    server = Server()
